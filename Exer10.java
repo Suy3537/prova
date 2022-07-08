520106ba15887ed0c747ecbbe9292376b08fc7b0
susu <susupower@gmail.com>
2022-07-08 19:58:46 -0300

    Create Exer10.java

---
+++ Exer10.java
@@ -0,0 +1,69 @@
+public class Exer10 {
+
+    public static void main(String[]args){
+
+        System.out.println("    ");
+
+System.out.println(" Verificar se a primeira expressão esta certa ou errada");
+System.out.println(" Expressão: 1 > 2");
+ if (1 > 2) {
+     System.out.println(" Verdadeiro");
+ } else {
+    System.out.println(" Falso");  
+ }
+System.out.println("   ");
+
+
+ System.out.println(" Verificar se a segunda expressão esta certa ou errada");
+ System.out.println(" Expressão: 8 = 8");
+ if (8 == 8) {
+    System.out.println( " Verdadeiro");
+} else {
+   System.out.println(" Falso");  
+}
+System.out.println("   ");
+
+
+System.out.println(" Verificar se a terceira expressão esta certa ou errada");
+System.out.println(" Expressão: 12-5>6");
+ if ( 12-5>6) {
+    System.out.println( " Verdadeiro");
+} else {
+   System.out.println(" Falso");  
+}
+System.out.println("   ");
+
+
+System.out.println(" Verificar se a quarta expressão esta certa ou errada");
+System.out.println(" Expressão: 0<3 && 8 < 9");
+ if (0<3 && 8 < 9) {
+    System.out.println( " Verdadeiro");
+} else {
+   System.out.println(" Falso");  
+}
+System.out.println("   ");
+
+Integer i = 9;
+System.out.println(" Verificar se a quinta expressão esta certa ou errada");
+System.out.println(" Expressão: i++ > i");
+
+ if ((i++) > i) {
+    System.out.println( " Verdadeiro");
+} else {
+   System.out.println(" Falso");  
+}
+System.out.println("   ");
+
+System.out.println(" Verificar se a sexta expressão esta certa ou errada");
+System.out.println(" Expressão: 10 * 90 / 50 - 2 == 16");
+ if (10 * 90 / 50 - 2 == 16) {
+    System.out.println( " Verdadeiro");
+} else {
+   System.out.println(" Falso");
+}
+System.out.println("   ");
+
+
+
+    }    
+}
